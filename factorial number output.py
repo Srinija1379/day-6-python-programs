bc1967f8c4d276a74d65d4d4ba951d341fca5db6
Srinija1379 <112627711+Srinija1379@users.noreply.github.com>
2022-10-08 10:40:41 +0530

    Add files via upload

---
+++ factorial number output.py
@@ -0,0 +1,23 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\factorial number.py 
+Factorial of 3 is 6
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\factorial number.py 
+Factorial of 4 is 24
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\factorial number.py 
+Factorial of 6 is 720
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\factorial number.py 
+Factorial of 6 is 720
+>>> 20
+20
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\factorial number.py 
+Factorial of 20 is 2432902008176640000
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\factorial number.py 
+Factorial of 30 is 265252859812191058636308480000000
+>>> 
---
+++ pythogeron triplet output.py
@@ -0,0 +1,32 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/pythogeron triplet.py 
+Enter upper limit:12
+3 4 5
+8 6 10
+>>> 
+ RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/pythogeron triplet.py 
+Enter upper limit:10
+3 4 5
+8 6 10
+>>> 
+ RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/pythogeron triplet.py 
+Enter upper limit:-10
+>>> 
+ RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/pythogeron triplet.py 
+Enter upper limit:#
+Traceback (most recent call last):
+  File "C:/Users/sse/AppData/Local/Programs/Python/Python37-32/pythogeron triplet.py", line 1, in <module>
+    limit=int(input("Enter upper limit:"))
+ValueError: invalid literal for int() with base 10: '#'
+>>> 
+ RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/pythogeron triplet.py 
+Enter upper limit:25
+3 4 5
+8 6 10
+5 12 13
+15 8 17
+12 16 20
+7 24 25
+>>> 
